283f96bf5c352bf755b49aeaa744460a7525aca6
PranavPBR <136674769+PranavPBR@users.noreply.github.com>
2023-07-20 21:42:18 -0700

    Add files via upload

---
+++ DungeonCrawler.java
@@ -0,0 +1,269 @@
+/*
+ *        Class: IT-516 - Data Structures & Algorithms
+ *
+ *
+ *   Compile: javac-algs4 DungeonCrawler.java
+ *
+ *       Run: java-algs4 DungeonCrawler [ a number between 1 and 4 ]
+ */
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.awt.*;
+import java.util.Random;
+
+
+public class DungeonCrawler {
+
+    private static final Color[] PLAYER_COLORS = {StdDraw.RED, StdDraw.BLUE, StdDraw.GREEN, StdDraw.ORANGE};
+    private static Integer[] playerSpace;
+
+    public static void main(String[] args) {
+        int numPlayers = Integer.parseInt(args[0]);    // takes input number of players from terminal
+        if (numPlayers < 1 || numPlayers > 4) {
+            StdOut.println("Min player 1 and Max 4");
+            return;
+        }
+        StdOut.println("Welcome to DungeonCrawler!");
+        StdOut.println("This will be a " + numPlayers + " player game.");
+        StdOut.println("The game begins now:");
+
+        playerSpace = new Integer[numPlayers];                           // Array to hold player's current space
+        int dieRolls = 0;                                                          // Count of all times the die is rolled
+        int[] dieRollsPalyer = new int[numPlayers];                               // Make an array to hold count of player's rolls
+        int[] dieRollResult = new int[6];                                         // Make an array to hold most common die rolls
+        int[][] dieRollPlayerResult = new int[numPlayers][6];
+        int[] climbed = new int[numPlayers];                                      // Make an array to hold number of ladders a player climbs
+        int[] slides = new int[numPlayers];                                       // Make an array to hold number of chutes a player slides down
+
+        for (int j = 0; j < numPlayers; j++) {                                     // Initialize all the values in all arrays to 0
+            playerSpace[j] = 0;
+        }
+
+
+        In in = new In("spaces.txt");                                              //  local file with Digraph data in it
+        Digraph spacesDigraph = new Digraph(in);
+        int winningSpace = spacesDigraph.V() - 1;
+
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setTitle("Dungeon Crawler");  // for setting the title window
+        StdDraw.setCanvasSize(1024, 540); // screen resolution
+        StdDraw.setScale(0, 10);
+        StdDraw.pause(1000);
+        StdDraw.clear();
+
+        // Main Game Loop
+        for (int i = 0; i < 1000000; i++) {                                        // Arbitrary big number of turns. We use "break" command at bottom to exit.
+            int player = i % numPlayers;                                           // Mathematically determines which player's turn it is by remainder
+            Random r = new Random();                                               // Create a random number holder
+            int roll = r.nextInt((6)) + 1;                                         // Create an integer to hold the random number & randomize
+            dieRollResult[roll - 1]++;
+            dieRollPlayerResult[player][roll - 1]++;
+
+            dieRolls++;
+            dieRollsPalyer[player]++;
+            // Increment times die has been rolled
+            //playerRolls[player]++;                                                 // Increment times player has rolled the die
+            // commonDieRolls[roll * numPlayers - 1]++;                               // Increment this die roll in the array of die rolls per player
+            StdOut.println("Player " + (player + 1) + " rolls a " + roll);
+            StdOut.println("         Position of Player : " + playerSpace[player] + ", runs additional + " + roll);
+            playerSpace[player] = playerSpace[player] + roll;// "Move" the player forward by the die roll
+
+            StdOut.println("         Moves to block " + playerSpace[player]);
+            if (playerSpace[player] < winningSpace) {
+                int numJumps = spacesDigraph.outdegree(playerSpace[player]);           // Check the digraph to see if there are any chutes or ladders
+                if (numJumps != 0) {            // and if there are (and player hasn't already moved past 100)
+                    for (Integer bigMove : spacesDigraph.adj(playerSpace[player])) {   // get ready to move them.
+                        if (playerSpace[player] < bigMove) {                           // If it's a ladder (player space is less than digraph space)
+                            playerSpace[player] = bigMove;                             // move them forward.
+                            StdOut.println("         Runs up to block " + bigMove);
+                            climbed[player]++;
+                        } else {                                                       // Otherwise, it's a chute (player space is more than digraph space)
+                            playerSpace[player] = bigMove;                             // so move them back.
+                            StdOut.println("         Runs down to block " + bigMove);
+                            slides[player]++;
+                        }
+                    }
+                }
+            }
+            if (playerSpace[player] >= winningSpace) {                             // If player's new space is greater than or equal to the win space
+                StdOut.println("Player " + (player + 1) + " wins!");               // display end of game message
+
+                StdDraw.clear();
+                StdDraw.pause(300);
+                StdDraw.picture(5, 5, "dungeon.jpg");
+                StdDraw.show();
+                StdDraw.text(5, 9, "Player " + (player + 1) + " wins!");
+                StdDraw.show();
+                StdDraw.pause(500);
+                break;                                                             // and exit the loop.
+            }
+
+            DungeonBoard(playerSpace, numPlayers);
+            StdDraw.show();
+            StdDraw.pause(100);
+        }
+
+        StdOut.println();
+        StdOut.println("End of game stats:");
+
+        StdOut.println();
+        StdOut.println("Total die rolls for the game " + dieRolls);
+
+        StdDraw.text(5, 8, "Total die rolls for the game " + dieRolls);
+        StdDraw.show();
+        StdDraw.pause(500);
+
+        int j = 7;
+        for (int z = 0; z < numPlayers; z++) {                                     // Loop through die roll stats
+            StdOut.println("Total die rolls for player " + (z + 1) + " is " + dieRollsPalyer[z]);   // Replace ??? with variable for player rolls
+
+
+            StdDraw.text(5, j, "Total die rolls for player " + (z + 1) + " is " + dieRollsPalyer[z]);
+            StdDraw.show();
+            j = j - 1;
+            StdDraw.pause(500);
+
+
+        }
+
+
+        Integer[] commonDieRollsResults = new Integer[6];                          // Create "6-sided" array for each die value
+        for (int w = 0; w < 6; w++) {                                              // Loop through common die roll results
+            commonDieRollsResults[w] = dieRollResult[w];                                          // You'll need to add all your player common die rolls here, rather than 0
+        }
+        int max = commonDieRollsResults[0];                                        // Create an integer to hold most-rolled die value
+        int index = 0;
+
+        for (int p = 1; p < 6; p++) {                                              // Find max value by looping through results
+            if (max < commonDieRollsResults[p]) {
+                max = commonDieRollsResults[p];
+                index = p;
+            }
+        }
+        StdOut.println();
+        StdOut.println("Most common die roll for the game is " + (index + 1));
+
+        StdDraw.text(5, j, "Most common die roll for the game is " + (index + 1));
+        StdDraw.show();
+        StdDraw.pause(500);
+        //StdDraw.clear();
+        j = j - 1;
+
+
+        for (int i = 0; i < numPlayers; i++) {
+            for (int w = 0; w < 6; w++) {                                              // Loop through common die roll results
+                commonDieRollsResults[w] = dieRollPlayerResult[i][w];                   // You'll need to add all your player common die rolls here, rather than 0
+            }
+            int max1 = commonDieRollsResults[0];
+            int index1 = 0;
+
+            for (int p = 1; p < 6; p++) {                                              // Loop through to find it
+                if (max1 < commonDieRollsResults[p]) {
+                    max1 = commonDieRollsResults[p];
+                    index1 = p;
+                }
+            }
+            StdOut.println("Most common die roll for player" + (i + 1) + " is " + (index1 + 1));
+
+            StdDraw.text(5, j, "Most common die roll for player" + (i + 1) + " is " + (index1 + 1));
+            //StdDraw.show();
+            //StdDraw.pause(500);
+            //
+            // StdDraw.clear();
+            j = j - 1;
+        }
+
+
+        StdOut.println();
+        int gameLadders = 0;
+        for (int l = 0; l < numPlayers; l++) {                                      // Sum game ladders climbed
+            gameLadders = gameLadders + climbed[l];                                          // Do a calculation here to replace 1
+        }
+        StdOut.println("Total treasures found for the game is " + gameLadders);
+        for (int l = 0; l < numPlayers; l++) {
+            StdOut.println("Total treasures found for player " + (l + 1) + " is " + climbed[l]);  // Do a calculation here to replace 0
+            j = j - 1;
+
+            StdDraw.text(5, j, "Total treasures found for player " + (l + 1) + " is " + (climbed[l]));
+            StdDraw.show();
+            StdDraw.pause(500);
+            j = j - 1;
+
+        }
+        StdOut.println();
+        int gameChutes = 0;                                                         // Sum game chutes slid
+        for (int l = 0; l < numPlayers; l++) {
+            gameChutes = gameChutes + slides[l];                                            // Do a calculation here to replace 1
+        }
+        StdOut.println("Total traps for the game is " + gameChutes);
+
+
+        for (int l = 0; l < numPlayers; l++) {
+            StdOut.println("Total traps for player " + (l + 1) + " is " + slides[l]);  // Do a calculation here to replace 0
+        }
+    }
+
+
+    public static void DungeonBoard(Integer[] playerSpace, int numPlayers) {
+        StdDraw.picture(5, 5, "dungeon.jpg");
+        for (int gridnumber = 0; gridnumber < 100; gridnumber++) {
+            int row = 9 - gridnumber / 10; // since Grid start from 0 need to take value 9
+            int col = gridnumber % 10;
+            if (row % 2 != 0) {
+                col = 9 - col;
+            }
+
+            double x = col + 0.5;
+            double y = row + 0.5;
+
+            StdDraw.picture(0.5, 9.5, "gate.png", 0.9, 0.9);
+            StdDraw.picture(4.5, 8.5, "Boulder.jpeg", 0.9, 0.9);
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line(0.5, 9.5, 4.5, 8.5);
+            StdDraw.picture(4.5, 3.5, "Spikes.jpeg", 0.9, 0.9);
+            StdDraw.picture(7.5, 5.5, "Spikes.jpeg", 0.9, 0.9);
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line(4.5, 3.5, 7.5, 5.5);
+            StdDraw.picture(2.5, 2.5, "fire.png", 0.9, 0.9);
+            StdDraw.picture(7.5, 4.5, "fire.png", 0.9, 0.9);
+            StdDraw.picture(6.5, 7.5, "rain.png", 0.9, 0.9);
+            StdDraw.line(2.5, 2.5, 6.5, 7.5);
+            StdDraw.line(7.5, 4.5, 6.5, 7.5);
+            StdDraw.picture(6.5, 1.5, "Boulder.jpeg", 0.9, 0.9);
+            StdDraw.picture(0.5, 2.5, "gate.png", 0.9, 0.9);
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line(6.5, 1.5, 0.5, 2.5);
+            StdDraw.picture(9.5, 0.5, "win.png", 0.9, 0.9);
+            StdDraw.picture(1.5, 2.5, "loot.jpg", 0.9, 0.9);
+            StdDraw.picture(1.5, 8.5, "loot.jpg", 0.9, 0.9);
+            StdDraw.setPenColor(StdDraw.GREEN);
+            StdDraw.line(1.5, 2.5, 1.5, 8.5);
+            StdDraw.picture(4.5, 5.5, "portal.png", 0.9, 0.9);
+            StdDraw.picture(3.5, 2.5, "portal.png", 0.9, 0.9);
+            StdDraw.setPenColor(StdDraw.GREEN);
+            StdDraw.line(4.5, 5.5, 3.5, 2.5);
+            StdDraw.picture(2.5, 6.5, "teleport.png", 0.9, 0.9);
+            StdDraw.picture(5.5, 2.5, "teleport.png", 0.9, 0.9);
+            StdDraw.line(2.5, 6.5, 5.5, 2.5);
+
+            // Modify color of the player to match their player color
+            for (int i = 0; i < numPlayers; i++) {
+                if (playerSpace[i] == gridnumber) {
+                    StdDraw.setPenColor(PLAYER_COLORS[i]);
+                    StdDraw.filledCircle(x, y, 0.25);
+                }
+            }
+
+            StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+            StdDraw.square(x, y, 0.5);
+            StdDraw.setPenColor(StdDraw.WHITE);
+            StdDraw.text(x, y, Integer.toString(gridnumber));
+
+
+        }
+    }
+}
